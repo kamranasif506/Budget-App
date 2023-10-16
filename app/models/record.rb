95e2ee1119db42b9834661908a04677481a47426
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-10-16 11:51:31 +0500

    linters error resolved

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,5 @@
 class Record < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :record_items
-    has_many :categories, through: :record_items
+  belongs_to :author, class_name: 'User'
+  has_many :record_items
+  has_many :categories, through: :record_items
 end
