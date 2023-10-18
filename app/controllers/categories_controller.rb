d965398693a6f15eb6b6db529e221315b9d1e4d0
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-10-18 23:08:56 +0500

    linters error fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,27 +1,28 @@
 class CategoriesController < ApplicationController
-    before_action :authenticate_user!
-    
-    def index
-      user = current_user.id
-      @categories = Category.where(author_id: user)
-    end
-    def new
-      @category = Category.new
-    end
+  before_action :authenticate_user!
 
-    def create
-      @category = Category.new(category_params)
-      @category.author = current_user
-      if @category.save
-        redirect_to categories_path, notice: 'Category was successfully created.'
-      else
-        render :new
-      end
-    end
+  def index
+    user = current_user.id
+    @categories = Category.where(author_id: user)
+  end
 
-    private
-    def category_params
-      params.require(:category).permit(:name, :icon)
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.author = current_user
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
-  
